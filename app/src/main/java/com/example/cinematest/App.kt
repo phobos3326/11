6f30317bba56236daa682df243a9c6b34bb2a1b6
phobos3326 <3326@bk.ru>
2024-11-23 22:17:27 +0700

    23_11_24 _1

--- app/src/main/java/com/example/cinematest/App.kt
+++ app/src/main/java/com/example/cinematest/App.kt
@@ -15,7 +15,6 @@ class App : Application() {
         startKoin {
             androidContext(this@App)
             modules(appModule)
-
             logger(PrintLogger(Level.INFO))
 
         }
---
+++ app/src/main/java/com/example/cinematest/ui/fragments/FilmItem.kt
@@ -0,0 +1,4 @@
+package com.example.cinematest.ui.fragments
+
+class FilmItem {
+}
